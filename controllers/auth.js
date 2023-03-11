045037c5c0abd0e3a9889a5bec4dc8a84f811716
Diego Urrego <diegourrg@gmail.com>
2023-03-11 00:46:22 -0500

    Se agrega middleware para peticiones json y se agrega lógica y validación de body de la petición.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,32 @@
 const { response } = require('express');
 
 const createUser = (req, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'El nombre debe tener al menos 5 letras',
+    });
+  }
+
   res.json({
     ok: true,
     msg: 'register',
+    name,
+    email,
+    password,
   });
 };
 
 const userLogin = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: 'login',
+    email,
+    password,
   });
 };
 
--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const app = express();
 // Directorio público
 app.use(express.static('public'));
 
+// Lectura y parseo del body
+app.use(express.json());
+
 // Rutas
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require('./routes/auth'));
 // TODO: auth // CRUD: Eventos
 
 // Escuchar peticiones
